70fa5294321a103e557c12d505f2a772e5494243
vrushalipr <50298146+vrushalipr@users.noreply.github.com>
2020-05-10 22:30:37 -0400

    Created table using thymeleaf

--- Manager/src/main/java/net/code/AppController.java
+++ Manager/src/main/java/net/code/AppController.java
@@ -1,7 +1,10 @@
 package net.code;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,8 +14,9 @@ public class AppController {
 	private ProductService service;
 	
 	@RequestMapping("/")
-	public String viewHome() {
-		
+	public String viewHome(Model model) {
+		List<Product> listProducts = service.listAll();
+		model.addAttribute("listProducts", listProducts);
 		return "index";
 	}
 }
