d6b9008a3713b2c4143dbc93c5ce9da61cc03f58
vrushalipr <50298146+vrushalipr@users.noreply.github.com>
2020-05-11 12:39:23 -0400

    Created new feature edit.

--- Manager/src/main/java/net/code/AppController.java
+++ Manager/src/main/java/net/code/AppController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -35,5 +37,15 @@ public class AppController {
 		service.save(product);
 		
 		return"redirect:/";
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductForm(@PathVariable(name= "id") int id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+		
+		Product product = service.get(id);
+		mav.addObject("product",product);
+		
+		return mav;
 	}
 }
